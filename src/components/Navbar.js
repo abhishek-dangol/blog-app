e90c77e422fa5572c4bf09cc82a55cd30d9ffd63
Abhishek Dangol <abhi.dangol@gmail.com>
2020-12-16 23:22:05 -0600

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,77 @@
+import { Avatar } from "@material-ui/core";
+import React, { useState } from "react";
+import { GoogleLogout } from "react-google-login";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  selectSignedIn,
+  selectUserData,
+  setInput,
+  setSignedIn,
+  setUserData,
+} from "../features/userSlice";
+import "../styling/navbar.css";
+import axios from "axios";
+
+const Navbar = () => {
+  const [inputValue, setInputValue] = useState("tech");
+  const isSignedIn = useSelector(selectSignedIn);
+  const userData = useSelector(selectUserData);
+
+  const dispatch = useDispatch();
+  const logout = (response) => {
+    dispatch(setSignedIn(false));
+    dispatch(setUserData(null));
+  };
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    dispatch(setInput(inputValue));
+  };
+
+  return (
+    <div className="navbar">
+      <h1 className="navbar__header">Dan's Blogs 💬</h1>
+      {isSignedIn && (
+        <div className="blog__search">
+          <input
+            className="search"
+            placeholder="Search for a blog"
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
+          />
+          <button className="submit" onClick={handleClick}>
+            Search
+          </button>
+        </div>
+      )}
+
+      {isSignedIn ? (
+        <div className="navbar__user__data">
+          <Avatar
+            className="user"
+            src={userData?.imageUrl}
+            alt={userData?.name}
+          />
+          <h1 className="signedIn">{userData?.givenName}</h1>
+          <GoogleLogout
+            clientId="899488485021-n8g7vq89jp5g1astp2j87f70dq6a87k7.apps.googleusercontent.com"
+            render={(renderProps) => (
+              <button
+                onClick={renderProps.onClick}
+                disabled={renderProps.disabled}
+                className="logout__button"
+              >
+                Logout
+              </button>
+            )}
+            onLogoutSuccess={logout}
+          />
+        </div>
+      ) : (
+        <h1 className="notSignedIn">User not available</h1>
+      )}
+    </div>
+  );
+};
+
+export default Navbar;
